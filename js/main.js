2304a40b33f2784e4d6c44c9866ff9e4a2cc8efe
Dina <dinakamel40000@gmail.com>
2022-08-17 22:19:19 +0200

    done with search

--- js/main.js
+++ js/main.js
@@ -53,10 +53,11 @@ function displaybookmarker() {
     Array.from(document.getElementsByClassName(`editinput`)).forEach((box) => {
       box.style.display = "none";
     });
-    Array.from(document.getElementsByClassName(`updatebtn`)).forEach((updatebox) => {
+    Array.from(document.getElementsByClassName(`updatebtn`)).forEach(
+      (updatebox) => {
         updatebox.style.display = "none";
-      });
-    
+      }
+    );
   } else {
     displaynobookmarker();
   }
@@ -126,20 +127,66 @@ function deleteItem(key) {
 
 // Update
 function updateItem(key) {
-    document.getElementById(`name${key}`).style.display = "none"
-    document.getElementById(`editinput${key}`).style.display = "inline-block"
-    document.getElementById(`editbtn${key}`).style.display = "none"
-    document.getElementById(`updatebtn${key}`).style.display = "inline-block"
+  document.getElementById(`name${key}`).style.display = "none";
+  document.getElementById(`editinput${key}`).style.display = "inline-block";
+  document.getElementById(`editbtn${key}`).style.display = "none";
+  document.getElementById(`updatebtn${key}`).style.display = "inline-block";
 }
 
-// save updates 
-function saveupdateItem(key){
-    console.log(document.getElementById(`editinputfield${key}`).value)
-    bookMarkListArray[key].name = document.getElementById(`editinputfield${key}`).value
-    document.getElementById(`name${key}`).style.display = "inline-block"
-    document.getElementById(`editinput${key}`).style.display =  "none"
-    document.getElementById(`editbtn${key}`).style.display = "inline-block"
-    document.getElementById(`updatebtn${key}`).style.display = "none"
-    displaybookmarker()
+// save updates
+function saveupdateItem(key) {
+  bookMarkListArray[key].name = document.getElementById(
+    `editinputfield${key}`
+  ).value;
+  document.getElementById(`name${key}`).style.display = "inline-block";
+  document.getElementById(`editinput${key}`).style.display = "none";
+  document.getElementById(`editbtn${key}`).style.display = "inline-block";
+  document.getElementById(`updatebtn${key}`).style.display = "none";
+  displaybookmarker();
 }
 
+// Search
+document.getElementById("search-input").onkeyup = function () {
+  var searchBookMarker = "";
+  if (this.value.length) {
+    bookMarkListArray.forEach((element, key) => {
+      console.log(element);
+      if (element.name.toLowerCase().includes(this.value.toLowerCase())) {
+        searchBookMarker += `
+            <div class="list-group-item mb-4 bg-light shadow ">
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-12 ">
+                    <div class="pt-2" id="name${key}">
+                        ${element.name}
+                    </div>
+                    <div class="pt-2 editinput" id="editinput${key}">
+                        <input type="text" value=" ${element.name}" class="form-control"  id="editinputfield${key}">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 ">
+                    <a class="btn btn-outline-info mt-1 btn-sm" href="${element.link}" target="_blank">Visit</a>
+                    <button class="btn btn-outline-success mt-1 btn-sm" onclick=updateItem(${key}) id="editbtn${key}">Edit</button>
+                    <button class="btn btn-outline-success mt-1 btn-sm updatebtn" onclick=saveupdateItem(${key}) id="updatebtn${key}">update</button>
+                    <button class="btn btn-outline-danger mt-1 btn-sm" onclick=deleteItem(${key})>Delete</button>
+                </div>
+            </div>
+        </div>
+            `;
+      }
+      document.getElementById("bookmarklist").innerHTML = searchBookMarker;
+
+      Array.from(document.getElementsByClassName(`editinput`)).forEach(
+        (box) => {
+          box.style.display = "none";
+        }
+      );
+      Array.from(document.getElementsByClassName(`updatebtn`)).forEach(
+        (updatebox) => {
+          updatebox.style.display = "none";
+        }
+      );
+    });
+  } else {
+    checkerdisplay();
+  }
+};
